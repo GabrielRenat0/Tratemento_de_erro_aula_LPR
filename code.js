3c19a3c6b77ee105208dd9ca85014e97cb8e5e6f
GabrielRenat0 <gabrielrenato382004@gmail.com>
2022-06-06 15:38:56 -0300

    Codigo com os requisitos pedidos

--- code.js
+++ code.js
@@ -5,6 +5,7 @@ const inputnumberButton = document.getElementById("inputnumberbutton");
 inputnumberButton.addEventListener("click", clickNumberButton);
 
 function clickButton(){
+
     arquivo = document.getElementById("inputfile");
 
     var fr=new FileReader();
@@ -19,14 +20,14 @@ function clickButton(){
 
         var info = document.getElementById('outputfile').textContent;
         if(info == null){
-            throw 'Informe um valor';
+            throw 'entre com um arquivo';
         }
-        
     }
     catch(error){
-        console.log(error);
+        error = 'Erro + erro javascript'
+        document.getElementById('outputnumber').innerHTML = error
+   
         alert(error);
-        //document.getElementById('output').textContent="Arquivo não localizado"
         }
 
     finally{
@@ -38,59 +39,42 @@ function clickButton(){
    function clickNumberButton(){
 
     number = document.getElementById("inputnumber").value
+
     try{
-        if(number == '')
-        throw 'Informe um valor;'
+        if(number == ''){
+        throw 'Informe um valor'
+        }
+        else if (number <= 5 || number >= 10 ){
+            throw 'Informe um valor maior que 5 e menor que 10'
+        }
     }
-
     catch(erro){
-document.getElementById("outputnumber").innerHTML = erro;
+
+        document.getElementById("outputnumber").innerHTML = erro
+         
     }
 
+    finally{
+        if(number != ''){
+            document.getElementById('outputnumbervalor').innerHTML = 'O número escolhido foi o '+number
+        }
+        else{
+            document.getElementById('outputnumbervalor').innerHTML = 'Nenhum valor foi inserido'
+        }
+    }
    console.log("Botao Number")
 }
     
-    
+ /*
+     1) Inserir um campo de input com a mensagem "informe um valor entre 5 a 10"
 
+     2) Verifica se é nulo, se for, lança exception "Informe um valor" 
 
+     3) Verifica se é maior que 5 e menor que 10, se não for, lança exception 
+     "Informe um valor maior que 5 e menor que 10"
 
+     4) Se o try falhar, lançar exception "Erro + erro javascript"
+     N entendi muito bem oq fazer nesse ponto
 
-   /* var fr=new FileReader();
-    
-    try{
-        
-        fr.readAsText(arquivo.files[0]);
-        fr.onload=function() {
-            info = fr.result;
-            document.getElementById('output').textContent=fr.result;
-        }
-
-        var info = document.getElementById('output').textContent;
-        if(info == null){
-            throw 'Informe um valor';
-        }
-        
-    }
-    catch(error){
-        console.log(error);
-        alert(error);
-        //document.getElementById('output').textContent="Arquivo não localizado"
-        }
-
-    finally{
-        alert("Obrigado pela visita")
-    }
-
-}
-
-/*fr.readAsText(arquivo.files[0]);
-
-    console.log(arquivo.files)
-    
-    fr.onload=function(){
-        document.getElementById('output').textContent=fr.result;
-    }
-    
-    
-}
+     5) No finnaly informar "O número escolhido foi + " numero
 */
